aae3e25fad588dfba74b76665805bd26e280dc50
Paulo Burke <pauloepburke@gmail.com>
2021-12-16 11:37:19 -0700

    Add examples

---
+++ examples/full_local_analysis.R
@@ -0,0 +1,59 @@
+
+library(PhenoComb)
+library(flowCore)
+
+options(PhenoComb.verbose = TRUE)
+
+# Paths to input files
+cell_file <- "path/to/cell/file" # .csv or .fcs
+channel_file <- "path/to/channel_file" # .csv
+sample_file <- "path/to/sample_file" # .csv
+
+
+# Reading data
+# Uncoment if it's a .fcs file
+#cell_data <- as.data.frame(flowCore::read.FCS(file.path(input_folder,flow_file),truncate_max_range = FALSE)@exprs)
+cell_data <- read.csv(cell_file) # comment if it's a .fcs file
+
+channel_data <- read.csv(channel_file)
+sample_data <- read.csv(sample_file)
+
+# Process raw dataset
+cell_data_processed <- process_cell_data(cell_data,
+                                         channel_data,
+                                         sample_data,
+                                         sampleID_col = "Sample_ID",
+                                         n_threads = 10
+                                         )
+
+# Generate combinatorial phenotypes
+comb_phenotypes <- combinatorial_phenotype_counts(cell_data_processed,
+                                                  parent_phen = NULL,
+                                                  max_phenotype_length = 4,
+                                                  min_count = 10,
+                                                  efficient = TRUE,
+                                                  n_threads = 10
+                                                  )
+
+
+
+# Filter statistically relevant phenotypes (Change groups_colum, g1, g2, and parent_phen accordingly to your data)
+relevant_phenotypes <- compute_statistically_relevant_phenotypes(comb_phenotypes,
+                                                                   channel_data,
+                                                                   sample_data,
+                                                                   groups_column = "Group",
+                                                                   g1 = "g1",
+                                                                   g2 = "g2",
+                                                                   max_pval = 0.05,
+                                                                   parent_phen = "Marker1+",
+                                                                   n_threads = 10
+)
+
+
+# Get independent statistically relevant phenotypes
+final_phenotypes <- get_independent_relevant_phenotypes(relevant_phenotypes,
+                                                        channel_data,
+                                                        n_phenotypes = 1000,
+                                                        min_confidence = 0.0,
+                                                        n_threads = 10
+)
---
+++ examples/full_server_analysis.R
@@ -0,0 +1,58 @@
+
+library(PhenoComb)
+
+
+input_folder <- "/path/to/input/folder"
+output_folder <- "path/to/output/folder"
+cell_file <- "cell_data.fcs" # or "cell_data.csv"
+channel_file <- "channel_data.csv"
+sample_file <- "sample_data.csv"
+
+
+
+# Process raw data and generates all combinatorial phenotypes
+combinatorial_phenotype_counts_server(file.path(input_folder,cell_file),
+                                      file.path(input_folder,channel_file),
+                                      file.path(input_folder,sample_file),
+                                      output_folder,
+                                      parent_phen = NULL,
+                                      min_count = 10,
+                                      max_phenotype_length = 0,
+                                      sampleID_col = "Sample_ID",
+                                      save_cell_data = TRUE,
+                                      continue = TRUE,
+                                      verbose = TRUE,
+                                      max_ram = 0,
+                                      efficient = TRUE,
+                                      n_threads = 10
+)
+
+# Filter statistically relevant phenotypes
+#(Change groups_colum, g1, g2, and parent_phen accordingly to your data)
+statistically_relevant_phenotypes_server(output_folder,
+                                         file.path(input_folder,channel_file),
+                                         file.path(input_folder,sample_file),
+                                         groups_column = "Group",
+                                         g1 = "g1",
+                                         g2 = "g2",
+                                         max_pval = 0.05,
+                                         parent_phen = "Marker1+",
+                                         continue = TRUE,
+                                         n_threads = 10,
+                                         verbose = TRUE)
+
+# Compute independent statistically relevant phenotypes
+# (Change groups_colum, g1, g2, and parent_phen accordingly to your data)
+get_independent_relevant_phenotypes_server(output_folder,
+                                           file.path(input_folder,channel_file),
+                                           file.path(input_folder,sample_file),
+                                           groups_column = "Group",
+                                           g1 = "g1",
+                                           g2 = "g2",
+                                           max_pval = 0.05,
+                                           parent_phen = "Marker1+",
+                                           n_phenotypes = 1000,
+                                           min_confidence = 0.0,
+                                           n_threads = 10,
+                                           verbose = TRUE
+)
