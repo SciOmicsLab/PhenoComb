a72b34a04ce7f0ecadee859b0372b1dac3782c17
Paulo Burke <pauloepburke@gmail.com>
2022-01-31 16:55:04 -0700

    try to fix max_phenotype_length bug

--- R/combinatorial_phenotypes_generation.R
+++ R/combinatorial_phenotypes_generation.R
@@ -130,7 +130,7 @@ generate_marker_combinations <- function(n_markers, max_phenotype_length = 0, lo
                                                                      mc.cores = n_threads)),]  
   }
   
-  if(nrow(local_marker_comb) == 0) return(NULL)
+  if(length(local_marker_comb) == 0) return(NULL)
   
   return(local_marker_comb)
   
