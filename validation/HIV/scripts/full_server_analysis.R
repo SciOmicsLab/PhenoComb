5c5a64550c2d4cf6b4efd3f4ad7c4ed646eebb0c
Paulo Burke <pauloepburke@gmail.com>
2022-03-25 11:04:41 -0600

    Uncomment code

--- validation/HIV/scripts/full_server_analysis.R
+++ validation/HIV/scripts/full_server_analysis.R
@@ -13,56 +13,40 @@ sample_file <- "ncell_filtered_sample_data.csv"
 
 
 # Process raw data and generates all combinatorial phenotypes
-# combinatorial_phenotype_counts_server(file.path(input_folder,cell_file),
-#                                       file.path(input_folder,channel_file),
-#                                       file.path(input_folder,sample_file),
-#                                       output_folder,
-#                                       parent_phen = NULL,
-#                                       min_count = 10,
-#                                       sample_fraction_min_counts = 0.5,
-#                                       max_phenotype_length = 0,
-#                                       sampleID_col = "Sample_ID",
-#                                       save_cell_data = TRUE,
-#                                       continue = TRUE,
-#                                       verbose = TRUE,
-#                                       max_ram = 0,
-#                                       efficient = TRUE,
-#                                       n_threads = 50
-# )
+combinatorial_phenotype_counts_server(file.path(input_folder,cell_file),
+                                      file.path(input_folder,channel_file),
+                                      file.path(input_folder,sample_file),
+                                      output_folder,
+                                      parent_phen = NULL,
+                                      min_count = 10,
+                                      sample_fraction_min_counts = 0.5,
+                                      max_phenotype_length = 0,
+                                      sampleID_col = "Sample_ID",
+                                      save_cell_data = TRUE,
+                                      continue = TRUE,
+                                      verbose = TRUE,
+                                      max_ram = 0,
+                                      efficient = TRUE,
+                                      n_threads = 50
+)
 
 # Filter statistically relevant phenotypes
-#(Change groups_colum, g1, g2, and parent_phen accordingly to your data)
-# statistically_relevant_phenotypes_server(output_folder,
-#                                          file.path(input_folder,channel_file),
-#                                          file.path(input_folder,sample_file),
-#                                          #output_file = "significant_phenotypes_CD3+_parent.csv",
-#                                          #log_file = "significant_phenotypes_CD3+_parent.log",
-#                                          test_type = "survival",
-#                                          survival_time_column = "survival_time_from_seroconversion",
-#                                          survival_status_column = "death",
-#                                          #parent_phen = "CD3+",
-#                                          max_pval = 1.0,
-#                                          continue = TRUE,
-#                                          n_threads = 50,
-#                                          verbose = TRUE)
-# 
-# 
-# statistically_relevant_phenotypes_server(output_folder,
-#                                          file.path(input_folder,channel_file),
-#                                          file.path(input_folder,sample_file),
-#                                          output_file = "significant_phenotypes_CD3+_parent.csv",
-#                                          log_file = "significant_phenotypes_CD3+_parent.log",
-#                                          test_type = "survival",
-#                                          survival_time_column = "survival_time_from_seroconversion",
-#                                          survival_status_column = "death",
-#                                          parent_phen = "CD3+",
-#                                          max_pval = 1.0,
-#                                          continue = TRUE,
-#                                          n_threads = 50,
-#                                          verbose = TRUE)
 
-# # Compute independent statistically relevant phenotypes
-# # (Change groups_colum, g1, g2, and parent_phen accordingly to your data)
+statistically_relevant_phenotypes_server(output_folder,
+                                         file.path(input_folder,channel_file),
+                                         file.path(input_folder,sample_file),
+                                         output_file = "significant_phenotypes_CD3+_parent.csv",
+                                         log_file = "significant_phenotypes_CD3+_parent.log",
+                                         test_type = "survival",
+                                         survival_time_column = "survival_time_from_seroconversion",
+                                         survival_status_column = "death",
+                                         parent_phen = "CD3+",
+                                         max_pval = 1.0,
+                                         continue = TRUE,
+                                         n_threads = 50,
+                                         verbose = TRUE)
+
+# Compute independent statistically relevant phenotypes
 get_independent_relevant_phenotypes_server(output_folder,
                                            file.path(input_folder,channel_file),
                                            file.path(input_folder,sample_file),
@@ -75,3 +59,4 @@ get_independent_relevant_phenotypes_server(output_folder,
                                            n_threads = 50,
                                            verbose = TRUE
 )
+1
